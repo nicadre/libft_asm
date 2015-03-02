99d85d5be94531278f9a9c2dee0598f9c7c0eff8
Nicolas CHEVALIER <niccheva@e1r4p15.42.fr>
2015-03-02 15:23:56 +0100

    remove warnings

--- srcs/ft_isalnum.s
+++ srcs/ft_isalnum.s
@@ -6,20 +6,18 @@
 ;    By: niccheva <niccheva@student.42.fr>          +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2015/02/28 23:11:33 by niccheva          #+#    #+#              ;
-;    Updated: 2015/02/28 23:59:18 by niccheva         ###   ########.fr        ;
+;    Updated: 2015/03/02 15:22:20 by niccheva         ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
+global _ft_isalnum
 extern _ft_isalpha
 extern _ft_isdigit
-global _ft_isalnum
 
 _ft_isalnum:
-	mov rax, _ft_isalpha
 	call _ft_isalpha
 	cmp rax, 1
 	je ok_isalnum
-	mov rax, _ft_isdigit
 	call _ft_isdigit
 	cmp rax, 1
 	je ok_isalnum
